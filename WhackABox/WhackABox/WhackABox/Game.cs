768ab75fd452df4658c1958354bb95015b51dbb2
Jimmy Engström <jimmy@apeoholic.se>
2020-05-03 14:30:04 +0200

    Adding the shared abstract Game class

---
+++ WhackABox/WhackABox/WhackABox/Game.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using Urho;
+using Urho.Shapes;
+namespace WhackABox
+{
+    public partial class Game : Application
+    {
+        private Scene scene;
+        public Game(ApplicationOptions options) : base(options)
+        {
+        }
+    }
+}
