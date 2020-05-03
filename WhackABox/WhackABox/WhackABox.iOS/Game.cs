565465af4cbf3c014e8418b0133b313519d6721a
Jimmy Engström <jimmy@apeoholic.se>
2020-05-03 14:45:37 +0200

    Defining the ARKitComponent

--- WhackABox/WhackABox/WhackABox.iOS/Game.cs
+++ WhackABox/WhackABox/WhackABox.iOS/Game.cs
@@ -1,10 +1,13 @@
-﻿using Urho;
+﻿using System.Linq;
+using ARKit;
+using Urho;
+using Urho.iOS;
 namespace WhackABox.iOS
 {
     public class Game : WhackABox.Game
     {
+        protected ARKitComponent arkitComponent;
         public Game(ApplicationOptions options) : base(options)
-        {
-        }
+        { }
     }
 }
