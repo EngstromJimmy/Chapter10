f6681e80979d326a9c09c7d083ebf702c528906c
Jimmy Engström <jimmy@apeoholic.se>
2020-05-03 14:43:32 +0200

    Adding platform-specific classes  Adding the iOS-specific class

---
+++ WhackABox/WhackABox/WhackABox.iOS/Game.cs
@@ -0,0 +1,10 @@
+﻿using Urho;
+namespace WhackABox.iOS
+{
+    public class Game : WhackABox.Game
+    {
+        public Game(ApplicationOptions options) : base(options)
+        {
+        }
+    }
+}
